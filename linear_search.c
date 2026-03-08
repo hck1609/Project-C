f363931bb0045661e202e592e9c527a92275d8ca
hck1609 <amarjeetgill1609@gmail.com>
2026-03-08 20:35:35 +0530

    modified:   linear_search.c

--- linear_search.c
+++ linear_search.c
@@ -3,18 +3,18 @@
 int linear_search(int LA[10], int ub, int item)
 {
     LA[ub+1] = item;
-    int loc = 1;
-    while(LA[loc] != item)
+    int location = 1;          //initialised searching location to 1
+    while(LA[location] != item)
     {
-        loc++;
+        location++;
     }
-    if (loc == ub+1)
+    if (location == ub+1)   //if location is ub+1, it means item is not found in the array
     {
-        return 0;
+        return 0;         //item not found
     }
     else
     {
-        return loc;
+        return location;  //item found at location
     }
 }
 void main()
@@ -28,18 +28,18 @@ scanf("%d",&ub);
 printf("\nEnter %d elements in the linear array:\n",ub);
 for(i=1;i<=ub;i++)
 {
-scanf("%d",&LA[i]);
+scanf("%d",&LA[i]);             //initialize array:LA
 }
 printf("\nThe elements stored in LA are:\n");
 for(i=1;i<=ub;i++)
 {
-printf("\nLA[%d]=%d",i,LA[i]);
+printf("\nLA[%d]=%d",i,LA[i]);  //traverse array:LA
 }
 printf("\n\nEnter the element you want to search:");
 scanf("%d",&item);
-loc = linear_search(LA,ub,item);
+loc = linear_search(LA,ub,item);    //assign returned value of linear_search to loc
 if(loc == 0){
-    printf("Item no found");
+    printf("Item not found");
 }
 else{
     printf("Item found at location %d (i.e. LA[%d])",loc,loc);
